816e9c7832ce12ff34c0ac90af7c4ee23b8c6c40
anaelle.fievez <anaelle.fievez@soprasteria.com>
2019-02-20 11:36:04 +0100

    feat : add callback to the database.

--- app/src/main/java/com/example/Gcc/data/source/local/GccRoomDatabase.java
+++ app/src/main/java/com/example/Gcc/data/source/local/GccRoomDatabase.java
@@ -1,9 +1,12 @@
 package com.example.Gcc.data.source.local;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 @Database(entities = {Event.class}, version = 1)
 public abstract class GccRoomDatabase extends RoomDatabase {
@@ -18,10 +21,39 @@ public abstract class GccRoomDatabase extends RoomDatabase {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             GccRoomDatabase.class, "gcc_database")
+                            .addCallback(sRoomDataBaseCallback)
                             .build();
                 }
             }
         }
         return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback sRoomDataBaseCallback =
+            new RoomDatabase.Callback() {
+                @Override
+                public void onOpen(@NonNull SupportSQLiteDatabase db) {
+                    super.onOpen(db);
+                    new PopulateDbAsync(INSTANCE).execute();
+                }
+            };
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void>{
+        private final EventDao dao;
+
+        PopulateDbAsync(GccRoomDatabase db){
+            dao = db.eventDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            dao.deleteAll();
+            Event event = new Event(1, "Hello event");
+            dao.insert(event);
+
+            Event event2 = new Event(2, "Hello event 2");
+            dao.insert(event2);
+            return null;
+        }
     }
 }
