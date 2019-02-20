48ac236929da1a70e5ae26541909729720ffba95
anaelle.fievez <anaelle.fievez@soprasteria.com>
2019-02-20 11:03:46 +0100

    feat : add recycler view adaptater.

---
+++ app/src/main/java/com/example/Gcc/EventListAdapter.java
@@ -0,0 +1,63 @@
+package com.example.Gcc;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.example.Gcc.data.source.local.Event;
+
+import java.util.List;
+
+public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.EventViewHolder>{
+
+    class EventViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView eventItemView;
+
+        private EventViewHolder(View itemView){
+            super(itemView);
+            eventItemView = itemView.findViewById(R.id.textViewRV);
+        }
+    }
+
+    private final LayoutInflater inflater;
+    private List<Event> eventList;
+
+    public EventListAdapter(Context context) {
+        inflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public EventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = inflater.inflate(R.layout.recyclerview_item, parent, false);
+        return new EventViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EventViewHolder holder, int position) {
+        if(eventList != null){
+            Event current = eventList.get(position);
+            holder.eventItemView.setText(current.getDescription());
+        } else {
+            holder.eventItemView.setText("No Event");
+        }
+    }
+
+    public void setEventList(List<Event> events){
+        eventList = events;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        if(eventList != null){
+            return eventList.size();
+        } else {
+            return 0;
+        }
+    }
+}
