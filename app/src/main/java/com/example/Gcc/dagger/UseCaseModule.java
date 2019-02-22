7f18dd37f6d5f61b5c46738d5b7a3d50fb7db347
anaelle.fievez <anaelle.fievez@soprasteria.com>
2019-02-22 16:43:38 +0100

    feat : @Inject Repository on UseCaseModule doesn't work.

--- app/src/main/java/com/example/Gcc/dagger/UseCaseModule.java
+++ app/src/main/java/com/example/Gcc/dagger/UseCaseModule.java
@@ -1,5 +1,6 @@
 package com.example.Gcc.dagger;
 
+import com.example.Gcc.data.source.local.EventRepository;
 import com.example.Gcc.usecase.ModifyEventUseCase;
 import dagger.Module;
 import dagger.Provides;
@@ -8,6 +9,13 @@ import javax.inject.Singleton;
 
 @Module
 public class UseCaseModule {
+
+    /*
+    @Provides
+    @Singleton
+    ModifyEventUseCase provideModifyEventUseCase(EventRepository eventRepository){
+        return new ModifyEventUseCase(eventRepository);
+    } */
 
     @Provides
     @Singleton
--- app/src/main/java/com/example/Gcc/usecase/ModifyEventUseCase.java
+++ app/src/main/java/com/example/Gcc/usecase/ModifyEventUseCase.java
@@ -3,18 +3,21 @@ package com.example.Gcc.usecase;
 import com.example.Gcc.data.source.local.EventRepository;
 import io.reactivex.Completable;
 
-import javax.inject.Inject;
-
 public class ModifyEventUseCase {
 
-    @Inject
     EventRepository eventRepository;
 
-    public Completable modifyEventAsync(){
+    /*
+    public ModifyEventUseCase(EventRepository eventRepository){
+        this.eventRepository = eventRepository;
+    }
+     */
+
+    public Completable modifyEventAsync() {
         return Completable.create(e -> modifyEvent());
     }
 
-    private void modifyEvent(){
+    private void modifyEvent() {
         eventRepository.getAllEvents();
         System.out.println("dodo");
     }
